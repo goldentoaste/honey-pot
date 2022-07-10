411c675ff212790c84c6d7329ba9cd03ad50bdcc
goldentoaste <rayg2375@gmail.com>
2022-07-10 16:42:21 -0600

    cache done

--- imageCache.py
+++ imageCache.py
@@ -1,14 +1,15 @@
 import os
-from typing import Dict, List
-import requests
-from requests.exceptions import RequestException
 from glob import glob
-import validators
+from typing import Dict
 
+import requests
+import validators
+from requests.exceptions import RequestException
 
 imageExt = {"image/bmp": ".bmp", "image/jpeg": ".jpg", "image/png": ".png"}
 
-
+# NOTE
+# save images has <abs(hash(fileURL))>.ext
 class CacheManager:
     def __init__(self, cacheFolder: str, cacheSize: int) -> None:
 
@@ -18,8 +19,12 @@ class CacheManager:
             os.mkdir(self.folder)
         self.images: Dict[str, str] = {}  # paths to images
 
-        for file in sum([glob(os.path.join(self.folder, f"*{ext}")) for ext in imageExt.values()], start=[]):
-            self.images[hash(file)] = file
+        # get .bmp, .jpg, or .png files in the cache folder, sorted by last modified date
+        for file in sorted(
+            sum((glob(os.path.join(self.folder, f"*{ext}")) for ext in imageExt.values()), start=[]),
+            key=os.path.getmtime,
+        ):
+            self.images[os.path.splitext(file)[0]] = file
 
     def getFile(self, url: str) -> str:
         """
@@ -27,7 +32,7 @@ class CacheManager:
         """
 
         # if url is cached, just return it
-        if hash(url) in self.images:
+        if str(abs(hash(url))) in self.images:
             return self.images[hash(url)]
 
         # try to download the link
@@ -39,11 +44,38 @@ class CacheManager:
             result = requests.get(url)
         except RequestException:
             print("Web request failed")
-            return
+            return None
+
         try:
             mimeType = result.headers["Content-Type"]
             ext = imageExt[mimeType]
-
+            filePath = os.path.join(self.folder, f"{abs(hash(url))}{ext}")
+            with open(filePath, "wb") as out:
+                out.write(result.content)
+            self.images[str(abs(hash(url)))] = filePath
+            self.removeExtra()
+            return filePath
         except KeyError:
-            print("link is not an image")
+            print("link is not an image, content type is not found")
             return None
+
+    def removeExtra(self):
+        """
+        removes the cached images that exceeds cache size limit
+        """
+        diff = len(self.images) - self.cacheSize
+        if diff > 0:
+            keys = list(self.images.keys())[0:diff]
+
+            for key in keys:
+                os.remove(self.images[key])
+                self.images.pop(key)
+
+    def clearCache(self):
+        """
+        clears the entire cache
+        """
+        oldSize = self.cacheSize
+        self.cacheSize = 0
+        self.removeExtra()
+        self.cacheSize = oldSize
